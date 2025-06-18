41646c637490a37ae8c33893112ebc0f6420f5bb
SabeehaMalikah <sabeeha.malikah@gmail.com>
2025-06-17 20:46:39 -0400

    new program for tuple generation based on Shioda's Lemma

---
+++ SM_code/pythoncode/ShiodaTuplesGenerator.py
@@ -0,0 +1,11 @@
+"""
+author -- Sabeeha Malikah
+date created -- 6/17/25
+last modified -- 6/17/25
+description -- We noticed that for m = p^2 and d = (p+1)/2, the indecomposable tuples are the same tuples that are
+               from Shioda's Lemma 5.5. This program will use the generalized equation to generate the necessary
+               indecomposable tuples for m = p^2.
+updates --
+
+"""
+
