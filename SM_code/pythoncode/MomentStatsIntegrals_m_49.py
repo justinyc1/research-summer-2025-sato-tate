error corrected, integrals were divided by wrong factor of 2pi

SM_code/pythoncode/MomentStatsIntegrals_m_49.py
@@ -35,7 +35,7 @@ def main():
     total_moment_stats = []
 
     for b in range(p*(p-1)):
-        print(f"b = {b}\n")
+        print(f"b = {b}: ", end = "")
         moment_stats = [1]
 
         if b == 0:
@@ -45,40 +45,35 @@ def main():
                     moment_stats.append(0)
                     continue
                 result = compute_initial_integral(n)
-                result *= ((2 ** n) / (2 * pi) ** normal_relations)
+                result *= ((2 ** n) / ((2 * pi) ** normal_relations))
                 print(f"{float(result):.4f}")
                 moment_stats.append(result)
             print()
             total_moment_stats.append(moment_stats)
-        elif b % 6 == 0 and b % 12 != 0:
-            for n in range(1, 9):
+        elif b % 6 == 0:
+            for n in range(1, 6):
                 if n % 2 != 0:
                     moment_stats.append(0)
                     continue
                 result = compute_other_integrals(n)
-                result *= (2 ** n) / (2 * pi)
+                result *= ((2 ** n) / ((2 * pi)**3))
                 moment_stats.append(result)
-            total_moment_stats.append(moment_stats)
-        elif b % 12 == 0:
-            for n in range(1, 9):
-                if n % 2 != 0:
-                    moment_stats.append(0)
-                    continue
-                result = compute_other_integrals(n)
-                result *= ((-2) ** n) / (2 * pi)
-                moment_stats.append(result)
+            print(moment_stats)
+            print()
             total_moment_stats.append(moment_stats)
         else:
-            for n in range(1, 9):
+            for n in range(1, 6):
                 moment_stats.append(0)
+            print(moment_stats)
+            print()
             total_moment_stats.append(moment_stats)
 
-    for item in total_moment_stats:
-        print(f"{item} \n")
+    # for item in total_moment_stats:
+    #     print(f"{item} \n")
 
     arr = np.array(total_moment_stats)
     column_averages = np.mean(arr, axis=0)
-    print([f"{float(x):.0f}" for x in column_averages])
+    print([f"{x}" for x in column_averages])
 
 
 main()

SM_code/pythoncode/MomentStatsIntegrals_m_9.py
@@ -1,68 +1,61 @@
-# USING SYMPY INTEGRATION
-
-from sympy import symbols, cos, pi, integrate, simplify, expand
+# USING SCIPY INTEGRATION
 import numpy as np
-
-def compute_initial_integral(n):
-    theta = symbols('theta1:4')  # theta1 to theta3
-
-    integrand = sum(cos(t) for t in theta)
-    integrand += cos(-theta[0] + theta[1] + theta[2])
-
-    integrand_power = expand(integrand**n)
-
-    for t in theta:
-        integrand_power = integrate(integrand_power, (t, 0, 2 * pi))
-
-    return simplify(integrand_power)
+from scipy import integrate
 
 
-def compute_other_integrals(n):
-    theta3 = symbols('theta3')
-    integrand = (cos(theta3))**n
-    integrand = expand(integrand)
-    integrand = integrate(integrand, (theta3, 0, 2 * pi))
-    return simplify(integrand)
+# m = 9, p = 3
 
+def generate_integrand(n):
+    def integrand(theta1, theta2, theta3):
+        return (np.cos(theta1) + np.cos(theta2) + np.cos(theta3) + np.cos(-theta1 + theta2 + theta3))**n
+    return integrand
+
+def f(n):
+    def function(theta3):
+        return np.cos(theta3)**n
+    return function
 
 def main():
     p = 3
     normal_relations = ((p ** 2 - 1) // 2) - ((p - 1) // 2)
     total_moment_stats = []
-
     for b in range(6):
-        print(f"b = {b}\n")
         moment_stats = [1]
-
         if b == 0:
             for n in range(1, 13):
                 if n % 2 != 0:
                     moment_stats.append(0)
                     continue
-                result = compute_initial_integral(n)
-                result *= ((2 ** n) / (2 * pi) ** normal_relations)
+                integrand = generate_integrand(n)
+                limits = [(0, 2*np.pi), (0, 2*np.pi), (0, 2*np.pi)]
+                result, _ = integrate.nquad(integrand, limits)
+                result *= ((2 ** n) / ((2 * np.pi) ** normal_relations))
                 moment_stats.append(result)
             total_moment_stats.append(moment_stats)
-        elif b % 2 != 0:
+        elif b%2 != 0:
             for n in range(1, 13):
                 moment_stats.append(0)
             total_moment_stats.append(moment_stats)
-        elif b == 2:
+        elif b==2:
             for n in range(1, 13):
-                if n % 2 != 0:
+                if n%2 != 0:
                     moment_stats.append(0)
                     continue
-                result = compute_other_integrals(n)
-                result *= (2 ** n) / (2 * pi)
+                function = f(n)
+                limits = [(0, 2*np.pi)]
+                result, _ = integrate.nquad(function, limits)
+                result *= (2 ** n) / (2 * np.pi)
                 moment_stats.append(result)
             total_moment_stats.append(moment_stats)
         else:
             for n in range(1, 13):
-                if n % 2 != 0:
+                if n%2 != 0:
                     moment_stats.append(0)
                     continue
-                result = compute_other_integrals(n)
-                result *= ((-2) ** n) / (2 * pi)
+                function = f(n)
+                limits = [(0, 2*np.pi)]
+                result, _ = integrate.nquad(function, limits)
+                result *= ((-2) ** n) / (2 * np.pi)
                 moment_stats.append(result)
             total_moment_stats.append(moment_stats)
 
@@ -76,67 +69,71 @@ def main():
 
 main()
 
+# USING SYMPY INTEGRATION
 
-
-
-# # USING SCIPY INTEGRATION
+# from sympy import symbols, cos, pi, integrate, simplify, expand
 # import numpy as np
-# from scipy import integrate
+
+# def compute_initial_integral(n):
+#     theta = symbols('theta1:4')  # theta1 to theta3
+#
+#     integrand = sum(cos(t) for t in theta)
+#     integrand += cos(-theta[0] + theta[1] + theta[2])
+#
+#     integrand_power = expand(integrand**n)
+#
+#     for t in theta:
+#         integrand_power = integrate(integrand_power, (t, 0, 2 * pi))
+#
+#     return simplify(integrand_power)
 #
 #
-# # m = 9, p = 3
+# def compute_other_integrals(n):
+#     theta3 = symbols('theta3')
+#     integrand = (cos(theta3))**n
+#     integrand = expand(integrand)
+#     integrand = integrate(integrand, (theta3, 0, 2 * pi))
+#     return simplify(integrand)
 #
-# def generate_integrand(n):
-#     def integrand(theta1, theta2, theta3):
-#         return (np.cos(theta1) + np.cos(theta2) + np.cos(theta3) + np.cos(-theta1 + theta2 + theta3))**n
-#     return integrand
-#
-# def f(n):
-#     def function(theta3):
-#         return np.cos(theta3)**n
-#     return function
 #
 # def main():
 #     p = 3
 #     normal_relations = ((p ** 2 - 1) // 2) - ((p - 1) // 2)
 #     total_moment_stats = []
+#
 #     for b in range(6):
+#         print(f"b = {b}\n")
 #         moment_stats = [1]
+#
 #         if b == 0:
 #             for n in range(1, 13):
 #                 if n % 2 != 0:
 #                     moment_stats.append(0)
 #                     continue
-#                 integrand = generate_integrand(n)
-#                 limits = [(0, 2*np.pi), (0, 2*np.pi), (0, 2*np.pi)]
-#                 result, _ = integrate.nquad(integrand, limits)
-#                 result *= ((2 ** n) / (2 * np.pi) ** normal_relations)
+#                 result = compute_initial_integral(n)
+#                 result *= ((2 ** n) / (2 * pi) ** normal_relations)
 #                 moment_stats.append(result)
 #             total_moment_stats.append(moment_stats)
-#         elif b%2 != 0:
+#         elif b % 2 != 0:
 #             for n in range(1, 13):
 #                 moment_stats.append(0)
 #             total_moment_stats.append(moment_stats)
-#         elif b==2:
+#         elif b == 2:
 #             for n in range(1, 13):
-#                 if n%2 != 0:
+#                 if n % 2 != 0:
 #                     moment_stats.append(0)
 #                     continue
-#                 function = f(n)
-#                 limits = [(0, 2*np.pi)]
-#                 result, _ = integrate.nquad(function, limits)
-#                 result *= (2 ** n) / (2 * np.pi)
+#                 result = compute_other_integrals(n)
+#                 result *= (2 ** n) / (2 * pi)
 #                 moment_stats.append(result)
 #             total_moment_stats.append(moment_stats)
 #         else:
 #             for n in range(1, 13):
-#                 if n%2 != 0:
+#                 if n % 2 != 0:
 #                     moment_stats.append(0)
 #                     continue
-#                 function = f(n)
-#                 limits = [(0, 2*np.pi)]
-#                 result, _ = integrate.nquad(function, limits)
-#                 result *= ((-2) ** n) / (2 * np.pi)
+#                 result = compute_other_integrals(n)
+#                 result *= ((-2) ** n) / (2 * pi)
 #                 moment_stats.append(result)
 #             total_moment_stats.append(moment_stats)
 #
@@ -145,7 +142,7 @@ main()
 #
 #     arr = np.array(total_moment_stats)
 #     column_averages = np.mean(arr, axis=0)
-#     print([f"{float(x):.0f}" for x in column_averages])
+#     print([f"{x}" for x in column_averages])
 #
 #
 # main()